24395bcf0f7e993abdadd15555e10894ae14f30f
man-ito-ba <pierre.m.global@gmail.com>
2016-06-24 17:51:11 +0200

    Plan général

---
+++ production_de_vent.ino
@@ -0,0 +1,16 @@
+/* anemometre production */
+// 2 boutons, qui incrémentent en 4, de 0 à 20.
+// un afficheur donne le résultat
+// le moteur envoie plus ou moins de vent
+
+
+/*
+
+3 parties :
+
+1. Consignes données au moteur PWM à partir de 2 boutons + et -
+2. Affichage des consignes sur deux afficheurs à 7 segments
+3. Accélération ou décélération du moteur PWM en fonction des consignes
+
+*/
+
